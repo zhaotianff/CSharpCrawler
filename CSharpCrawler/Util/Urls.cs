1222e8cf9ed5935e9a9fb500009be4dbf39c475e
zhaotianff <zhaotianff@163.com>
2018-07-26 12:42:31 +0800

    Add Popular Search Engine Url

---
+++ CSharpCrawler/Util/Urls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpCrawler.Util
+{
+    class Urls
+    {
+        public const string BaiduUrl = "http://www.baidu.com/s?wd=%s";
+    }
+}

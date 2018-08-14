dbaeabb57ce5da08fb8ca90b1d2d9d51f74def21
zhaotianff <zhaotianff@163.com>
2018-08-14 15:23:52 +0800

    我又回来了

--- CSharpCrawler/MainWindow.xaml.cs
+++ CSharpCrawler/MainWindow.xaml.cs
@@ -22,12 +22,63 @@ namespace CSharpCrawler
         public MainWindow()
         {
             InitializeComponent();
+            InitializeCommands();
         }
 
-        private void btn_GetHtmlSourceCode_Click(object sender, RoutedEventArgs e)
+        #region Commands
+        private void InitializeCommands()
         {
-            GetHtmlSourceCode page = new GetHtmlSourceCode();
-            this.main.Content = page;
+            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, CloseWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, MaximizeWindow, CanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, MinimizeWindow, CanMinimizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, RestoreWindow, CanResizeWindow));
+            CommandBindings.Add(new CommandBinding(SystemCommands.ShowSystemMenuCommand, ShowSystemMenu));
         }
+
+        private void CanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ResizeMode == ResizeMode.CanResize || ResizeMode == ResizeMode.CanResizeWithGrip;
+        }
+
+        private void CanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = ResizeMode != ResizeMode.NoResize;
+        }
+
+        private void CloseWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+            //SystemCommands.CloseWindow(this);
+        }
+
+        private void MaximizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void MinimizeWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void RestoreWindow(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
+
+
+        private void ShowSystemMenu(object sender, ExecutedRoutedEventArgs e)
+        {
+            var element = e.OriginalSource as FrameworkElement;
+            if (element == null)
+                return;
+
+            var point = WindowState == WindowState.Maximized ? new Point(0, element.ActualHeight)
+                : new Point(Left + BorderThickness.Left, element.ActualHeight + Top + BorderThickness.Top);
+            point = element.TransformToAncestor(this).Transform(point);
+            SystemCommands.ShowSystemMenu(this, point);
+        }
+        #endregion
+
     }
 }
---
+++ CSharpCrawler/Util/WebUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCrawler.Util
+{
+    class WebUtil
+    {
+        public static bool IsResourceAvailable(string url)
+        {
+            return true;
+        }
+    }
+}

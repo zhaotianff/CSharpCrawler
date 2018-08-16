8c325dae43e5f638fbf47411d535f6bfc6705f94
zhaotianff <zhaotianff@163.com>
2018-08-16 16:21:27 +0800

    没有提交信息

---
+++ CSharpCrawler/Model/HttpHeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCrawler.Model
+{
+    class HttpHeader
+    {
+        public string ContentEncoding { get; set; }
+
+        public long ContentLength { get; set; }
+
+        public string ContentType { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public string Server { get; set; }
+
+        public string CharSet { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}

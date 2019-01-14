2c3d98a4ab15be5ab2a884eecfb165026d1f7943
ZTI <zhaotianff@163.com>
2019-01-14 11:21:40 +0800

    更新了截图

---
+++ CSharpCrawler/Model/NodeStruct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCrawler.Model
+{
+    /// <summary>
+    /// 结点结构(DOM树显示)
+    /// </summary>
+    public class NodeStruct
+    {    
+        public string DisplayName { get; set; }
+        public string InnerText { get; set; }        
+        public string InnerHtml { get; set; }             
+        public List<NodeStruct> Children { get; set; }
+        public NodeStruct()
+        {
+            Children = new List<NodeStruct>();
+        }
+    
+    }
+}

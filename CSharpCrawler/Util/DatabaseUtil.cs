3f3550ceb9ab32accfc614305daf0260409cfd7a
ZTI <zhaotianff@163.com>
2019-03-04 17:32:13 +0800

    添加数据库实用类

---
+++ CSharpCrawler/Util/DatabaseUtil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCrawler.Util
+{
+    public class DatabaseUtil
+    {
+        public static string DatatableToHtml(DataTable dt)
+        {
+            //try
+            //{
+
+            //    int row = 1;
+            //    StringBuilder html = new StringBuilder("<table>");
+            //    while (reader.Read())
+            //    {
+            //        if (row == 1)
+            //        {
+            //            html.Append("<tr>");
+            //            for (int i = 1; i < reader.FieldCount + 1; ++i)
+            //            {
+            //                html.Append($"<td>{reader.GetName(i - 1)}</td>");
+            //            }
+
+            //            html.Append("</tr>");
+            //        }
+
+            //        html.Append("<tr>");
+            //        for (int j = 1; j < reader.FieldCount + 1; ++j)
+            //        {
+            //            html.Append($"<td>{reader.GetValue(j - 1)}</td>");
+            //        }
+
+            //        html.Append("</tr>");
+            //        row++;
+            //    }
+
+            //    html.Append("</table>");
+
+            //    return html.ToString();
+            //}
+            //finally
+            //{
+
+            //}
+            return "";
+        }
+    }
+}

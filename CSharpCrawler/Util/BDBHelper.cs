7087fd13f72e058a45283bb53701d05692f9dd52
ZTI <zhaotianff@163.com>
2019-08-31 11:01:55 +0800

    增加Berkeley DB使用代码

--- CSharpCrawler/Util/BDBHelper.cs
+++ CSharpCrawler/Util/BDBHelper.cs
@@ -3,10 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BerkeleyDB;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace CSharpCrawler.Util
 {
     public class BDBHelper
     {
+        private BTreeDatabase db;
+
+        public BDBHelper(string dbPath)
+        {
+            BTreeDatabaseConfig bTreeDatabaseConfig = new BTreeDatabaseConfig();
+            //文件不存在则创建
+            bTreeDatabaseConfig.Creation = CreatePolicy.IF_NEEDED;
+            //页大小
+            bTreeDatabaseConfig.PageSize = 512;
+            //缓存大小
+            bTreeDatabaseConfig.CacheSize = new CacheInfo(0, 64 * 1024, 1);
+
+            db = BTreeDatabase.Open(dbPath, bTreeDatabaseConfig);
+        }
+
+        ~BDBHelper()
+        {
+            if (db != null)
+                db.Close();
+        }
+
+        public void Put(DatabaseEntry key,DatabaseEntry value)
+        {
+            db.Put(key, value);
+        }
+
+        public void Put(DatabaseEntry key,byte[] data)
+        {
+            db.Put(key, new DatabaseEntry(data));
+        }
+
+        public void Put(DatabaseEntry key,string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open))
+            {
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    byte[] data = br.ReadBytes((int)fs.Length);                    
+                    Put(key, data);
+                }
+            }
+        }
+
+        public KeyValuePair<DatabaseEntry,DatabaseEntry> Get(DatabaseEntry key)
+        {
+            return db.Get(key);
+        }
+
+        public T Get<T>(DatabaseEntry key)
+        {
+            T t = default(T);
+            BinaryFormatter formatter = new BinaryFormatter();        
+            MemoryStream memStream;           
+            var pair = db.Get(key);
+            memStream = new MemoryStream(pair.Value.Data.Length);
+            memStream.Write(pair.Value.Data, 0, pair.Value.Data.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            t = (T)formatter.Deserialize(memStream);
+            memStream.Close();
+            return t;
+        }
+
+        public Dictionary<DatabaseEntry,DatabaseEntry> FetchAll()
+        {
+            BTreeCursor cursor = db.Cursor();
+            Dictionary<DatabaseEntry, DatabaseEntry> dic = new Dictionary<DatabaseEntry, DatabaseEntry>();
+            
+            while(cursor.MoveNext())
+            {
+                dic.Add(cursor.Current.Key, cursor.Current.Value);
+            }
+
+            cursor.Close();
+            return dic;
+        }
+
+        public void Clear()
+        {
+            BTreeCursor cursor = db.Cursor();           
+
+            while (cursor.MoveNext())
+            {
+                db.Delete(cursor.Current.Key);
+            }
+            cursor.Close();
+        }
     }
 }
--- CSharpCrawler/Views/DataStorage.xaml.cs
+++ CSharpCrawler/Views/DataStorage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BerkeleyDB;
+using CSharpCrawler.Util;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,51 +25,106 @@ namespace CSharpCrawler.Views
     /// </summary>
     public partial class DataStorage : Page
     {
+        private const string DBPath = "demo.db";
+
+        BDBHelper bdb;
+
         public DataStorage()
         {
+            //更详细的使用方式，可以访问
+            //https://www.oracle.com/database/technologies/related/berkeleydb-downloads.html
+            //下载源码包，里面有C#使用示例
+            //这里只是介绍基本使用
             InitializeComponent();
+
+            bdb = new BDBHelper(DBPath);
         }
 
         private void btn_WriteStringToBDB_Click(object sender, RoutedEventArgs e)
         {
-            BTreeDatabaseConfig bTreeDatabaseConfig = new BTreeDatabaseConfig();
-            //文件不存在则创建
-            bTreeDatabaseConfig.Creation = CreatePolicy.IF_NEEDED;
-            //页大小
-            bTreeDatabaseConfig.PageSize = 512;
-            //缓存大小
-            bTreeDatabaseConfig.CacheSize = new CacheInfo(0, 64 * 1024, 1);
-            BTreeDatabase bTreeDatabase = BTreeDatabase.Open("demo.db", bTreeDatabaseConfig);
             string guid = this.tbox_Key.Text;
             string url = this.tbox_Value.Text;
             DatabaseEntry key = new DatabaseEntry();
             key.Data = Encoding.ASCII.GetBytes(guid);
             DatabaseEntry value = new DatabaseEntry();
             value.Data = Encoding.ASCII.GetBytes(url);
-            bTreeDatabase.Put(key, value);
-            EMessageBox.Show("写入成功");
-            bTreeDatabase.Close();         
+            bdb.Put(key, value);
+            EMessageBox.Show("写入成功");       
         }
 
         private void btn_ReadStringFromBDB_Click(object sender, RoutedEventArgs e)
         {
-            //KeyValuePair<DatabaseEntry, DatabaseEntry> pair = bTreeDatabase.Get(key);
-            //EMessageBox.Show(Encoding.ASCII.GetString(pair.Key.Data));
+            var guid = this.tbox_Key.Text;
+            var key = new DatabaseEntry();
+            key.Data = Encoding.ASCII.GetBytes(guid);
+            KeyValuePair<DatabaseEntry, DatabaseEntry> pair = bdb.Get(key);
+            EMessageBox.Show(Encoding.ASCII.GetString(pair.Value.Data));
         }
 
         private void btn_WriteObjectToBDB_Click(object sender, RoutedEventArgs e)
         {
             var key = new DatabaseEntry();
-            key.Data = Encoding.ASCII.GetBytes(Guid.NewGuid().ToString());
+            key.Data = Encoding.ASCII.GetBytes("TestClassKey");
             CrawlerRecord crawlerRecord = new CrawlerRecord() { id = 1,content = "<html><body><h1>Hello World</h1></body></html>",title = "helloworld",url = "https://myfreetime.cn"};
             var data = new DatabaseEntry();
             data.Data = crawlerRecord.GetBytes();
             //写入操作
+            bdb.Put(key, data);
+            EMessageBox.Show("写入成功");
         }
 
         private void btn_ReadObjectFromBDB_Click(object sender, RoutedEventArgs e)
         {
+            var key = new DatabaseEntry();
+            key.Data = Encoding.ASCII.GetBytes("TestClassKey");
+            CrawlerRecord record = bdb.Get<CrawlerRecord>(key);
 
+            if(record != null)
+            {
+                EMessageBox.Show($"ID = {record.id},Url = {record.url},Title = {record.title},Content = {record.content}");
+            }
+        }
+
+        private void btn_WriteImageToBDB_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+            fileDialog.Filter = "图像文件|*.jpg;*.png;*.bmp;|全部文件|*.*";
+            fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (fileDialog.ShowDialog().Value == true)
+            {
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(fileDialog.FileName);
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.EndInit();
+                this.img.Source = bi;
+
+                var key = new DatabaseEntry();
+                key.Data = Encoding.ASCII.GetBytes("TestImageKey");
+                bdb.Put(key, fileDialog.FileName);
+
+                EMessageBox.Show("写入成功");
+            }
+        }
+
+        private void btn_ReadImageFromBDB_Click(object sender, RoutedEventArgs e)
+        {
+            var key = new DatabaseEntry();
+            key.Data = Encoding.ASCII.GetBytes("TestImageKey");
+            var pair = bdb.Get(key);
+
+            //可以直接保存文件
+            //System.IO.File.WriteAllBytes("test.jpg", pair.Value.Data);
+       
+            //显示在界面上
+            BitmapImage bi = new BitmapImage();
+            System.IO.MemoryStream ms = new MemoryStream(pair.Value.Data);          
+            bi.BeginInit();
+            bi.CacheOption = BitmapCacheOption.OnLoad;
+            bi.StreamSource = ms;
+            bi.EndInit();
+            this.img.Source = bi;
+            ms.Close();
         }
     }
 

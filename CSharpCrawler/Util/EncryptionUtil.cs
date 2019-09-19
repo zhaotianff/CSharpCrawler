c83f990c186a6320eaa9e7f1321b61133d747fb0
ZTI <zhaotianff@163.com>
2019-09-19 08:34:12 +0800

    EncryptionUtil

---
+++ CSharpCrawler/Util/EncryptionUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCrawler.Util
+{
+    public class EncryptionUtil
+    {
+        public static string MD5_32(string str)
+        {
+            byte[] strBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            strBytes = new System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(strBytes);
+            var strVal = "";
+            for (int i = 0; i < strBytes.Length; i++)
+            {
+                strVal += strBytes[i].ToString("x").PadLeft(2, '0');
+            }
+            return strVal;
+        }
+    }
+}

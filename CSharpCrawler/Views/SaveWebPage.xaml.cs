db90f97ed8c6f86d9ed4f4feea61acadf42efe5f
zhaotianff <zhaotianff@DESKTOP-VPOI8TT>
2020-08-20 18:04:33 +0800

    完成保存网页为图片功能

--- CSharpCrawler/Views/SaveWebPage.xaml.cs
+++ CSharpCrawler/Views/SaveWebPage.xaml.cs
@@ -28,12 +28,30 @@ namespace CSharpCrawler.Views
         }
 
 
-        private void btn_SaveAsImage_Click(object sender, RoutedEventArgs e)
+        private async void btn_SaveAsImage_Click(object sender, RoutedEventArgs e)
         {
-            var fileName = Environment.CurrentDirectory + "\\download\\web.jpg";
-            var result = ImageUtil.SaveControlContentAsImage(this, fileName);
+            //如果这一步发生异常了，可以参考 https://www.cnblogs.com/zhaotianff/p/13528507.html
+            await new PuppeteerSharp.BrowserFetcher().DownloadAsync(PuppeteerSharp.BrowserFetcher.DefaultRevision);
 
-            if (result == true)
+            var browser = await PuppeteerSharp.Puppeteer.LaunchAsync(new PuppeteerSharp.LaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();  //打开一个新标签
+            await page.GoToAsync(this.tbox_Url.Text); //访问页面
+
+            //设置截图选项
+            PuppeteerSharp.ScreenshotOptions screenshotOptions = new PuppeteerSharp.ScreenshotOptions();
+            //screenshotOptions.Clip = new PuppeteerSharp.Media.Clip() { Height = 0, Width = 0, X = 0, Y = 0 };//设置截剪区域
+            screenshotOptions.FullPage = true; //是否截取整个页面
+            screenshotOptions.OmitBackground = false;//是否使用透明背景，而不是默认白色背景
+            screenshotOptions.Quality = 100; //截图质量 0-100（png不可用）
+            screenshotOptions.Type = PuppeteerSharp.ScreenshotType.Jpeg; //截图格式
+
+            var fileName = Environment.CurrentDirectory + "\\download\\web.jpg";
+            await page.ScreenshotAsync(fileName, screenshotOptions);
+
+            if (System.IO.File.Exists(fileName))
             {
                 BitmapImage bi = new BitmapImage();
                 bi.BeginInit();
@@ -45,6 +63,9 @@ namespace CSharpCrawler.Views
             {
                 EMessageBox.Show("保存网页截图失败");
             }
+
+            browser.Disconnect();
+            browser.Dispose();
         }
     }
 }
